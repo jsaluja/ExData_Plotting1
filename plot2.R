3b86b93a909691e2940d1123cff3e0ee5745c72e
Jaspal Singh Saluja <jaspal.saluja@gmail.com>
2014-07-13 19:24:39 -0400

    added comments

--- plot2.R
+++ plot2.R
@@ -1,3 +1,10 @@
+# Note 1. The package install is required just once not everytime
+# Note 2. library command is required whenever we need to use read.csv.sql
+# Note 3. File has been downloaded and unzipped in the working directory, the script works on the file 
+
+ install.packages("sqldf")
+ library(sqldf)
+
  fileData <- read.csv.sql("household_power_consumption.txt",sql="SELECT * FROM file WHERE Date='1/2/2007' OR Date='2/2/2007'",sep=";",header=T,colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"),stringsAsFactors=FALSE)
  fileData$DateTime <- strptime(paste(fileData$Date,fileData$Time),"%d/%m/%Y %H:%M:%S")
  
