eb7449bb25763b1d5ef8a690e4a5ab6ccc5b1ea4
Jaspal Singh Saluja <jaspal.saluja@gmail.com>
2014-07-14 10:20:13 -0400

    Note 4 comment to explain where clause

--- plot4.R
+++ plot4.R
@@ -1,7 +1,7 @@
 # Note 1. The package install is required just once not everytime
 # Note 2. library command is required because read.csv.sql is present in sqldf package
 # Note 3. File has been downloaded and unzipped in the working directory, the script works on the file 
-
+# Note 4: read.csv.sql filters the data with where condition, data for '1/2/2007' '2/2/2007' is loaded into fileData
  install.packages("sqldf")
  library(sqldf)
 
